f76817cf2b1078d9da7ecf3208401f29c87e987e
EzyhB <ezyhbmail@gmail.com>
2022-02-22 16:24:17 +0000

    edited the verel json file 1

--- db/scripts/createTable.js
+++ db/scripts/createTable.js
@@ -2,10 +2,12 @@
 exports.__esModule = true;
 var __1 = require("..");
 var createBlogPostTable = function () {
-    return (0, __1["default"])("CREATE TABLE IF NOT EXISTS blogposts (id serial PRIMARY KEY, blog_date DATE NOT NULL DEFAULT CURRENT_DATE, blog_author TEXT, blog_title TEXT, blog_content TEXT);");
+  return (0, __1["default"])(
+    "CREATE TABLE IF NOT EXISTS blogposts (id serial PRIMARY KEY, blog_date DATE NOT NULL DEFAULT CURRENT_DATE, blog_author TEXT, blog_title TEXT, blog_content TEXT);"
+  );
 };
 createBlogPostTable();
-// DATE NOT NULL DEFAULT CURRENT_DATE
+// NOT NULL DEFAULT CURRENT_DATE
 // const createBlogCommentsTable = ()=>{
 //     return query('CREATE TABLE IF NOT EXISTS blogcomments ()')
 // }
