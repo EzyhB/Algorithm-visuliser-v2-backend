ea680d50a1628756bb2719f0c9b8acba7b11f76b
EzyhB <ezyhbmail@gmail.com>
2022-03-05 17:30:29 +0000

    added user image section

--- db/scripts/createTable.js
+++ db/scripts/createTable.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 var __1 = require("..");
 var createBlogPostTable = function () {
-    return (0, __1["default"])("DROP TABLE IF EXISTS blogposts; CREATE TABLE IF NOT EXISTS blogposts (id serial PRIMARY KEY, blog_date timestamp default CURRENT_TIMESTAMP, blog_author TEXT, blog_title TEXT, blog_content TEXT);");
+    return (0, __1["default"])("DROP TABLE IF EXISTS blogposts; CREATE TABLE IF NOT EXISTS blogposts (id serial PRIMARY KEY, blog_date timestamp default CURRENT_TIMESTAMP, blog_image TEXT, blog_author TEXT, blog_title TEXT, blog_content TEXT);");
 };
 createBlogPostTable();
 // DATE NOT NULL DEFAULT CURRENT_DATE
